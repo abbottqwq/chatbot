ac01ba750de05f20c4bde1c153d8b7e025f3463f
lizhuohui <li.zhuohui@northeastern.edu>
2021-04-12 02:02:20 -0400

    add skaffold to test without stupid docker update

---
+++ message-controller/app.py
@@ -0,0 +1,8 @@
+from flask import Flask, url_for, request, jsonify
+import config
+app = Flask(__name__)
+@app.route('/', methods=['GET', 'POST'])
+def hello():
+    return "Hello World!"
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80, debug=config.DEBUG)
