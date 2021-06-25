f00e0aa39dc06f602d61a53b34832de27799523c
lizhuohui <li.zhuohui@northeastern.edu>
2021-06-25 17:10:23 -0400

    lastest -> latest damnnnnnnnnnnnnnnnnnnn

--- deploy.sh
+++ deploy.sh
@@ -1,19 +1,19 @@
 PWD
 ls
-docker build -t abbottqwq/chatbot-client:lastest -t abbottqwq/chatbot-client:$SHA -f ./chatbot-client/Dockerfile ./chatbot-client
-docker build -t abbottqwq/chatbot-api:lastest -t abbottqwq/chatbot-api:$SHA -f ./chatbot-api/Dockerfile ./chatbot-api
-# docker build -t abbottqwq/chatbot-client:lastest -t abbottqwq/chatbot-client:$SHA -f ./chatbot-client/Dockerfile ./chatbot-client
-docker build -t abbottqwq/chatbot-server:lastest -t abbottqwq/chatbot-server:$SHA -f ./chatbot-server/Dockerfile ./chatbot-server
+docker build -t abbottqwq/chatbot-client:latest -t abbottqwq/chatbot-client:$SHA -f ./chatbot-client/Dockerfile ./chatbot-client
+docker build -t abbottqwq/chatbot-api:latest -t abbottqwq/chatbot-api:$SHA -f ./chatbot-api/Dockerfile ./chatbot-api
+# docker build -t abbottqwq/chatbot-client:latest -t abbottqwq/chatbot-client:$SHA -f ./chatbot-client/Dockerfile ./chatbot-client
+docker build -t abbottqwq/chatbot-server:latest -t abbottqwq/chatbot-server:$SHA -f ./chatbot-server/Dockerfile ./chatbot-server
 
-docker build -t abbottqwq/mongo-worker:lastest -t abbottqwq/mongo-worker:$SHA -f ./mongo-worker/Dockerfile ./mongo-worker
-docker build -t abbottqwq/redis-worker:lastest -t abbottqwq/redis-worker:$SHA -f ./redis-worker/Dockerfile ./redis-worker
+docker build -t abbottqwq/mongo-worker:latest -t abbottqwq/mongo-worker:$SHA -f ./mongo-worker/Dockerfile ./mongo-worker
+docker build -t abbottqwq/redis-worker:latest -t abbottqwq/redis-worker:$SHA -f ./redis-worker/Dockerfile ./redis-worker
 
-docker push abbottqwq/chatbot-api:lastest
-docker push abbottqwq/chatbot-client:lastest
-docker push abbottqwq/chatbot-server:lastest
+docker push abbottqwq/chatbot-api:latest
+docker push abbottqwq/chatbot-client:latest
+docker push abbottqwq/chatbot-server:latest
 
-docker push abbottqwq/mongo-worker:lastest
-docker push abbottqwq/redis-worker:lastest
+docker push abbottqwq/mongo-worker:latest
+docker push abbottqwq/redis-worker:latest
 
 docker push abbottqwq/chatbot-api:$SHA
 docker push abbottqwq/chatbot-client:$SHA
@@ -26,8 +26,8 @@ kubectl apply -f namespace
 kubectl apply -f k8s
 
 
-kubectl set image deployments/chatbot-api-deployment chatbot-api=abbottqwq/chatbot-api:lastest
-kubectl set image deployments/chatbot-client-deployment chatbot-api=abbottqwq/chatbot-client:lastest
-kubectl set image deployments/chatbot-server-deployment chatbot-api=abbottqwq/chatbot-server:lastest
-kubectl set image deployments/redis-worker-deployment chatbot-api=abbottqwq/rediskubectl set image deployments/redis-worker:lastest
-kubectl set image deployments/mongo-worker-deployment chatbot-api=abbottqwq/mongo-worker:lastest
+kubectl set image deployments/chatbot-api-deployment chatbot-api=abbottqwq/chatbot-api:latest
+kubectl set image deployments/chatbot-client-deployment chatbot-api=abbottqwq/chatbot-client:latest
+kubectl set image deployments/chatbot-server-deployment chatbot-api=abbottqwq/chatbot-server:latest
+kubectl set image deployments/redis-worker-deployment chatbot-api=abbottqwq/rediskubectl set image deployments/redis-worker:latest
+kubectl set image deployments/mongo-worker-deployment chatbot-api=abbottqwq/mongo-worker:latest
